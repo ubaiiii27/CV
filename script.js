6eb5b6c34cff417fe6d126b75465780aede63e89
Ubaidilla <ubaidilla764@gmail.com>
2025-05-21 23:31:20 +0700

    Menambahkan file baru untuk proyek

---
+++ script.js
@@ -0,0 +1,34 @@
+//tamplate efek transisi menampilkan section yang transparan
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("section1").style.opacity = "1";
+    }, 600);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("section2").style.opacity = "1";
+    }, 1020);
+});
+
+//ini adalah fungsi show image di tengah layar
+function showImage(img) {
+    let modal = document.getElementById("imageModal");
+    let modalImg = document.getElementById("modalImg");
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+}
+
+//ini adalah fungsi untuk menutup image
+function closeImage() {
+    document.getElementById("imageModal").style.display = "none";
+}
+ //ini adalah transisi untuk menampilkan sertifikat
+document.addEventListener("DOMContentLoaded", function() {
+    let certificates = document.querySelector(".certificates");
+    
+    if (certificates) {
+        setTimeout(function() {
+            certificates.classList.add("show");
+        }, 1000);
+    }
+});
